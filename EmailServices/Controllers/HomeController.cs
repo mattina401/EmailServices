8c67dba77193e1b0c95f691a6ba9776bdbf1bfeb
mattina401 <mattina401@gmail.com>
2016-11-04 19:08:37 -0700

    initial UI and DB

---
+++ EmailServices/Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using EmailServices.Models;
+
+namespace EmailServices.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Account
+        public ActionResult Messages()
+        {
+            using (MessageDb db = new MessageDb())
+            {
+                return View(db.message.ToList());
+            }
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserAccount account)
+        {
+            if (ModelState.IsValid)
+            {
+                using (OurDbContext db = new OurDbContext())
+                {
+                    db.userAccount.Add(account);
+                    db.SaveChanges();
+                }
+
+                ModelState.Clear();
+                ViewBag.Message = account.FirstName + " " + account.LastName + "successfully registered.";
+            }
+
+            return View();
+        }
+
+        //Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(UserAccount user)
+        {
+            using (OurDbContext db = new OurDbContext())
+            {
+                var usr = db.userAccount.Single(u => u.Username == user.Username && u.Password == user.Password);
+                if (usr != null)
+                {
+                    Session["UserID"] = usr.UserID.ToString();
+                    Session["Username"] = usr.Username.ToString();
+                    return RedirectToAction("LoggedIn");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or Password is wrong.");
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult LoggedIn()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult LoggedIn(Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                using (MessageDb db = new MessageDb())
+                {
+                    db.message.Add(message);
+                    db.SaveChanges();
+                }
+
+                ModelState.Clear();
+                ViewBag.Message = message.Receiver + " " + message.Subject + "successfully stored.";
+            }
+
+            return View();
+        }
+    }
+}
---
+++ EmailServices/Models/UserAccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace EmailServices.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserID { get; set; }
+        
+        [Required(ErrorMessage = "First Name is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3]\.)|(([\w-]+\.)+))([a-zA-Z{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter valid email.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Please confirm your password.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+
+
+    public class Message
+    {
+        [Key]
+        public int MessageID { get; set; }
+
+        [Required(ErrorMessage = "Receiver's email address is required.")]
+        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3]\.)|(([\w-]+\.)+))([a-zA-Z{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter valid email.")]
+        public string Receiver { get; set; }
+
+        [Required(ErrorMessage = "Subject is required.")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Context is required.")]
+        public string Context { get; set; }
+    }
+}
